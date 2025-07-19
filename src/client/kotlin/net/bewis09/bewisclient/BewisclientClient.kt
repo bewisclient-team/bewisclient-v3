aa269ea5bdffbafcf57b341c043778e77d840618
Bewis09 <bewis09minecraft@gmail.com>
2025-07-19 13:40:10 +0200

    Initial commit

---
+++ src/client/kotlin/net/bewis09/bewisclient/BewisclientClient.kt
@@ -0,0 +1,9 @@
+package net.bewis09.bewisclient
+
+import net.fabricmc.api.ClientModInitializer
+
+object BewisclientClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/net/bewis09/bewisclient/BewisclientDataGenerator.kt
@@ -0,0 +1,9 @@
+package net.bewis09.bewisclient
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object BewisclientDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
