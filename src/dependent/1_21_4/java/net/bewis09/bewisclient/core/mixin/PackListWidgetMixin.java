2826a12cb7e6e632d04a3b4454f752de56c8b02e
Bewis09 <bewis09minecraft@gmail.com>
2026-01-18 18:05:27 +0100

    refactor: Update access widener and fix PackListWidget position logic

--- src/dependent/1_21_4/java/net/bewis09/bewisclient/core/mixin/PackListWidgetMixin.java
+++ src/dependent/1_21_4/java/net/bewis09/bewisclient/core/mixin/PackListWidgetMixin.java
@@ -3,13 +3,14 @@ package net.bewis09.bewisclient.core.mixin;
 import net.bewis09.bewisclient.impl.settings.functionalities.PackAdderSettings;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.pack.PackListWidget;
-import net.minecraft.client.gui.screen.pack.PackScreen;
 import net.minecraft.client.gui.widget.AlwaysSelectedEntryListWidget;
 import net.minecraft.client.gui.widget.ThreePartsLayoutWidget;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.Objects;
 
 @Mixin(PackListWidget.class)
 public abstract class PackListWidgetMixin extends AlwaysSelectedEntryListWidget<PackListWidget.ResourcePackEntry> {
@@ -23,6 +24,6 @@ public abstract class PackListWidgetMixin extends AlwaysSelectedEntryListWidget<
 
     @Override
     public void position(int width, ThreePartsLayoutWidget layout) {
-        this.position(width, (!PackAdderSettings.INSTANCE.isEnabled() || title != PackScreen.AVAILABLE_TITLE) ? layout.getContentHeight() : layout.getContentHeight() - 20, layout.getHeaderHeight());
+        this.position(width, (!PackAdderSettings.INSTANCE.isEnabled() || !Objects.equals(title.getString(), Text.translatable("pack.available.title").getString())) ? layout.getContentHeight() : layout.getContentHeight() - 20, layout.getHeaderHeight());
     }
 }
